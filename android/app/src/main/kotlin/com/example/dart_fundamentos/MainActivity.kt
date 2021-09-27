14ba2a09f184984858a2e5a0e074bb4a5644347f
leo <realleo2009@hotmail.com>
2021-09-27 17:56:04 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/dart_fundamentos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dart_fundamentos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/1_variaveis/1_variaveis.dart
@@ -0,0 +1,24 @@
+void main() {
+
+  num qualquerNumero = 1;
+  num qualquerNumero2 = 1.23897;
+  int idade = 37;
+  double valorIphone = 12.000;
+  String nomeCurso = 'Curso Flutter';
+
+  String curso;
+  curso = 'academia flutter';
+
+  //identifica o valor e atrabui um tipo
+  var numero = 1;
+  var texto = 'sfjsfns';
+
+  //recebe qualquer valor, igual o VAR.
+  Object objetoQualquer = 1.62;
+  dynamic dynamicQualquer = 1;
+
+
+
+
+
+}
