2c5c86a0c383cdc871b7fd77f98ae6a4091550be
leo <realleo2009@hotmail.com>
2021-09-28 17:36:17 -0300

    modificadores

---
+++ lib/2_modificadores/1_modificadores.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  //não pode ser alterado depois da inicialização , imutavel.
+  //pode atribuir o valor
+  //tempo de execução.
+  final nomeCompletoFinal = 'leonardo';
+  final nomeCompletoFinal2 = nomeCompletoFinal;
+
+  //não pode ser alterado depois da inicialização
+  //deve ter valores fixos no momento de compilação. tem que atrabuir o valor da
+  //variavel sempre ao iniciá-la
+  //tempo de compilação
+  const nomeCompletoConst = 'leonardo';
+  const nomeCompletoConst2 = nomeCompletoConst;
+}
