3585eca756ae3ebc29b7530d1233145bf8c3f069
leo <realleo2009@hotmail.com>
2021-11-01 17:40:09 -0300

    Conversões e Manipulações

--- lib/7_conversoes_manipulacoes/2_manipulando_numericos.dart
+++ lib/7_conversoes_manipulacoes/2_manipulando_numericos.dart
@@ -1,3 +1,34 @@
 void main() {
+  final idade = 30;
+  print('Sua idade é $idade'); //concatenação evita o uso do ''+ e .toString
+
+  ///
+  final valor = -20;
+
+  if (valor.isNegative) {
+    print(valor);
+  }
+
+  ///
+  final valorDouble = 10.65;
+  print(valorDouble.round()); //arredonda
+  print(valorDouble.roundToDouble()); // arredonda com .0
+
+  ///
+  final valorStringCerto = '30';
+  final valorStringErrado = 'Leo';
+
+  final valorInt = int.parse(valorStringCerto);  //converte a string em numero
+  final valorInt2 = int.tryParse(valorStringErrado);
+  print(valorInt);
+  print(valorInt2);
+  if (valorInt2 != null) {
+    print('O valor é ${valorInt2 + 2}');
+  }
+
+  ///
+  final precocamisa = 30.5468785;
+  print(precocamisa.toStringAsFixed(2));
 
 }
+
